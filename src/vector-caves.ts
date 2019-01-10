aaf8aa441f0aa9c93c11363bb048fcb66af5dbe5
Jeremy Burns <j@jaburns.net>
2019-01-09 19:22:32 -0700

    More style stuff and update bundle.

--- src/vector-caves.ts
+++ src/vector-caves.ts
@@ -1,9 +1,8 @@
 export const initPost = () :void => {
-    const helloButton = document.getElementById('hello-button') as HTMLButtonElement;
     const firstCanvas = document.getElementById('first-canvas') as HTMLCanvasElement;
     const ctx = firstCanvas.getContext('2d') as CanvasRenderingContext2D;
 
-    helloButton.onclick = () => {
+    firstCanvas.onclick = () => {
         ctx.fillStyle = '#000';
         ctx.fillRect(
             30*Math.floor(Math.random()*10),
