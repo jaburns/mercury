1434fc46ec641731ab6d0dc921cfdee3a2aa4cf7
Jeremy Burns <j@jaburns.net>
2019-02-16 16:40:18 -0700

    Added socket.io

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const path = require('path');
 const express = require('express');
-
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 const PORT = process.env.PORT || '3000';
 
 app.use(express.static(path.join(__dirname, './docs')));
 app.get('/', (req, res) => res.redirect('/index.html'));
-app.listen(PORT, () => console.log('Listening on '+PORT));
+
+io.on('connection', socket => {
+    console.log('User connected');
+});
+
+http.listen(PORT, () => console.log('Listening on '+PORT));
